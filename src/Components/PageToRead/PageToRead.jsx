35f443f165c05815b777435ef3bbd5bdca9957e0
tamimtarik <tamimtarik15@gmail.com>
2024-12-16 22:40:03 +0600

    Update Node Modules

--- src/Components/PageToRead/PageToRead.jsx
+++ src/Components/PageToRead/PageToRead.jsx
@@ -1,36 +1,68 @@
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
+
 import { useEffect, useState } from "react";
 import { getStoredBooks } from "../../Utils/localStorage";
-import { useLoaderData } from "react-router-dom";
-import BookChart from "../BookChart/BookChart";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+let dataChart = [];
 
 
-const PageToRead = () => {
-    const books = useLoaderData();
-    const [listedBooks, setListedBooks] = useState([]);
+const data = getStoredBooks();
 
-    useEffect(() => {
-        const storedBookIds = getStoredBooks();
-        if (books.length > 0) {
-            const bookListed = [];
-            for (const id of storedBookIds) {
-                const book = books.find((book) => book.id === id);
-                if (book) {
-                    bookListed.push(book)
-                }
-            }
-            setListedBooks(bookListed);
-            
-        }
-    }, [books]);
-    
-    
-    return (
-        <div>
-            {
-                listedBooks.map(item => <BookChart key={item.id} item={item}></BookChart>)
-            }
-        </div>
-    );
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${
+    y + height / 3
+  }
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+    x + width
+  }, ${y + height}
+  Z`;
 };
 
-export default PageToRead;
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+export default function PagesToRead() {
+  const localDataStore = getStoredBooks();
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    setChartData(localDataStore);
+  },[]);
+
+  dataChart = chartData;
+  return (
+    <div className="flex justify-center items-center">
+      <BarChart
+        width={1000}
+        height={500}
+        data={chartData}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="bookName" />
+        <YAxis />
+        <Bar
+          dataKey="totalPages"
+          fill="#8884d8"
+          shape={<TriangleBar />}
+          label={{ position: "top" }}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+      </BarChart>
+    </div>
+  );
+}
